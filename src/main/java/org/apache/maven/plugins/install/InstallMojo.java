8a258d2274534700a4a9b00c5b01fc4c3e34a23d
Tamas Cservenak <tamas@cservenak.net>
2023-03-21 14:12:26 +0100

    Reformat

--- src/main/java/org/apache/maven/plugins/install/InstallMojo.java
+++ src/main/java/org/apache/maven/plugins/install/InstallMojo.java
@@ -206,16 +206,15 @@ public class InstallMojo extends AbstractMojo {
                 request.addArtifact(RepositoryUtils.toArtifact(mavenMainArtifact));
             } else if (!project.getAttachedArtifacts().isEmpty()) {
                 if (allowIncompleteProjects) {
-                    getLog().warn( "" );
-                    getLog().warn( "The packaging plugin for this project did not assign" );
-                    getLog().warn( "a main file to the project but it has attachments. Change packaging to 'pom'." );
-                    getLog().warn( "" );
-                    getLog().warn( "Incomplete projects like this will fail in future Maven versions!" );
-                    getLog().warn( "" );
-                } else
-                {
-                    throw new MojoExecutionException( "The packaging plugin for this project did not assign "
-                            + "a main file to the project but it has attachments. Change packaging to 'pom'." );
+                    getLog().warn("");
+                    getLog().warn("The packaging plugin for this project did not assign");
+                    getLog().warn("a main file to the project but it has attachments. Change packaging to 'pom'.");
+                    getLog().warn("");
+                    getLog().warn("Incomplete projects like this will fail in future Maven versions!");
+                    getLog().warn("");
+                } else {
+                    throw new MojoExecutionException("The packaging plugin for this project did not assign "
+                            + "a main file to the project but it has attachments. Change packaging to 'pom'.");
                 }
             } else {
                 throw new MojoExecutionException(
